c8a9bfee990e339483c3a8d2598984391b955f2f
TheBlockBender <floris.jolink@gmail.com>
2018-07-15 15:13:24 +0200

    initial commit

---
+++ src/main/java/me/theblockbender/nature/sounds/NatureSounds.java
@@ -0,0 +1,22 @@
+/*
+ * NatureSounds plugin - Adds ambient sounds to Minecraft.
+ * Copyright (C) 2018 Floris Jolink (TheBlockBender / JustDJplease) - All Rights Reserved
+ *
+ * You are allowed to:
+ * - Modify this code, and use it for personal projects. (Private servers, small networks)
+ * - Take ideas and / or formats of this plugin and use it for personal projects. (Private servers, small networks)
+ *
+ * You are NOT allowed to:
+ * - Resell the original plugin or a modification of it.
+ * - Claim this plugin as your own.
+ * - Distribute the source-code or a modification of it without prior consent of the original author.
+ *
+ */
+
+package me.theblockbender.nature.sounds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NatureSounds extends JavaPlugin {
+
+}
